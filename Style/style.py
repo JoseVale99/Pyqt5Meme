a16182a91c4b416f8509fe94c2fc2d355d1e5bfb
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2022-04-25 00:14:14 -0500

    ✅ Adding: style for app

---
+++ Style/style.py
@@ -0,0 +1,44 @@
+style="""
+QLabel{
+  background: transparent;
+  color: #FFF;
+  font-weight: bold;
+  font-family: Arial;
+  font-size: 19px;
+}
+QCheckBox{
+  background: transparent;
+  border-bottom: none;
+  font-size: 12px;
+  color:#fff;
+  font-family:sans-serif;
+}
+QPushButton#btnYes{
+  background: #1469B8;
+  color:#FFF;
+  border-radius: 25px;
+  font-family: Arial;
+  font-size: 15px;
+}
+QPushButton#btnYes:pressed {
+  background: #2388E6;
+  color:#FFF;
+  border-radius: 25px;
+  font-family: Arial;
+  font-size: 15px;
+}
+QPushButton#btnNo{
+  background: #D80F0F;
+  color:#FFF;
+  border-radius: 25px;
+  font-family: Arial;
+  font-size: 15px;
+}
+QPushButton#btnNo:pressed {
+  background: #FF2C2C;
+  color:#FFF;
+  border-radius: 25px;
+  font-family: Arial;
+  font-size: 15px;
+}
+"""
